b7beda73e2df628b0710b4a888c0f59f39402426
tetzng <ffffffffffffunk@gmail.com>
2019-09-13 13:13:16 +0900

    fixed templates

--- ft.py
+++ ft.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from keras.models import Model
 from keras.applications.vgg16 import VGG16
 from keras.models import Sequential
@@ -11,18 +12,15 @@ nb_classes = len(classes)
 
 img_width, img_height = 200, 200
 
-# トレーニング用とバリデーション用の画像格納先
 train_data_dir = './data/train/'
 validation_data_dir = './data/test/'
 
-# 今回はトレーニング用に200枚、バリデーション用に50枚の画像を用意した。
 nb_train_samples = 400
 nb_validation_samples = 400
 
 batch_size = 16
 nb_epoch = 20
 
-# トレーンング用、バリデーション用データを生成するジェネレータ作成
 train_datagen = ImageDataGenerator(
   rescale=1.0 / 255,
   zoom_range=0.2,
@@ -48,30 +46,24 @@ validation_generator = validation_datagen.flow_from_directory(
   batch_size=batch_size,
   shuffle=True)
 
-  # VGG16のロード。FC層は不要なので include_top=False
 input_tensor = Input(shape=(img_width, img_height, 3))
 vgg16 = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 
-# FC層の作成
 top_model = Sequential()
 top_model.add(Flatten(input_shape=vgg16.output_shape[1:]))
 top_model.add(Dense(256, activation='relu'))
 top_model.add(Dropout(0.5))
 top_model.add(Dense(nb_classes, activation='softmax'))
 
-# VGG16とFC層を結合してモデルを作成
 vgg_model = Model(input=vgg16.input, output=top_model(vgg16.output))
 
-# 最後のconv層の直前までの層をfreeze
 for layer in vgg_model.layers[:15]:
     layer.trainable = False
 
-# 多クラス分類を指定
 vgg_model.compile(loss='categorical_crossentropy',
           optimizer=SGD(lr=1e-4, momentum=0.9),
           metrics=['accuracy'])
 
-# Fine-tuning
 history = vgg_model.fit_generator(
     train_generator,
     samples_per_epoch=nb_train_samples,
@@ -79,8 +71,6 @@ history = vgg_model.fit_generator(
     validation_data=validation_generator,
     nb_val_samples=nb_validation_samples)
 
-# 重みを保存
 vgg_model.save_weights('vgg16_finetuning_weights.h5')
 
-# モデルを保存
 vgg_model.save('vgg16_finetuning_model.h5')
--- web.py
+++ web.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import tensorflow as tf
 import multiprocessing as mp
 
@@ -9,33 +9,26 @@ from werkzeug import secure_filename
 import os
 import eval
 
-# 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 app.config['DEBUG'] = True
-# 投稿画像の保存先
 UPLOAD_FOLDER = './static/images/tmp'
 
-# ルーティング。/にアクセス時
 @app.route('/')
 def index():
   return render_template('index.html')
 
-# 画像投稿時のアクション
 @app.route('/post', methods=['GET','POST'])
 def post():
   if request.method == 'POST':
     if not request.files['file'].filename == u'':
-      # アップロードされたファイルを保存
       f = request.files['file']
       img_path = os.path.join(UPLOAD_FOLDER, secure_filename(f.filename))
       f.save(img_path)
-      # eval.pyへアップロードされた画像を渡す
       result = eval.evaluation(img_path, './model.ckpt')
     else:
       result = []
     return render_template('index.html', result=result)
   else:
-    # エラーなどでリダイレクトしたい場合
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
